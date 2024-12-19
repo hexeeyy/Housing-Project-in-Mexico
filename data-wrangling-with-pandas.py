66d2a74c8862793dfa7cc39317d50240d947c519
hexeeyy <hexilonpayno@gmail.com>
2024-12-20 02:35:02 +0800

    Commented lines of code

--- data-wrangling-with-pandas.py
+++ data-wrangling-with-pandas.py
@@ -1,1 +1,16 @@
+import pandas as pd
+#from IPython.display import VimeoVideo 
+#VimeoVideo("656321516", h="e85e3bf248", width=600)
+"""df1 = pd.read_csv("data/mexico-real-estate-1.csv")
+df2 = pd.read_csv("data/mexico-real-estate-2.csv")
+df3 = pd.read_csv("data/mexico-real-estate-3.csv")
 
+# Print object type and shape for DataFrames
+print("df1 type:", type(df1))
+print("df1 shape:", df1.shape)
+print()
+print("df2 type:", type(df2))
+print("df2 shape:", df2.shape)
+print()
+print("df3 type:", type(df3))
+print("df3 shape:", df3.shape)"""
